8475997b1c5d3ec2543fe724fe682efc5f28112d
meqdad <meqdad.moradi@outlook.com>
2022-07-30 16:41:53 +0200

    data fetched from real website

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
 import "./assets/style/style.css";
 import divider from "./assets/images/pattern-divider-desktop.svg";
 import dice from "./assets/images/icon-dice.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-   const [advice, setAdvice] = useState([
-      "It is easy to sit up and take notice, what is difficult is geting up and taking action.",
-      "Ad mea simul timeam evertitur, no mei congue cotidieque. Ex duo alii principes assueverit, an percipit liberavisse quo.",
-      "Meis nonumy quaeque ea cum, per cibo movet posidonium cu. Sint ponderum quo in.",
-      "Eos te justo postulant, mei nulla probatus efficiendi ut, qui alia gubergren vituperata ne. Dicam deserunt duo ut.",
-      "It's a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation.",
-      "Vel esse nostrud cu, ne omnes labore temporibus nec, quo posse invidunt ad. Vel ea equidem invidunt.",
-      "Eum cetero conclusionemque ei, in suas mentitum eum. Diam nullam eum no, dicant principes maiestatis eum an.",
-      "Ut ius munere tritani, qui fabulas iudicabit at. Sea brute vivendum ei. Duo ex feugiat repudiare reformidans.",
-      "Eu mea mucius laoreet elaboraret. Meis nihil vitae cu vim, vim ne nisl hinc iracundia. Stet voluptatibus id per.",
-      "It's also called placeholder (or filler) text. It's a convenient tool for mock-ups.",
-   ]);
+   const [advice, setAdvice] = useState(null);
 
-   const [newAdvice, setNewAdvice] = useState(null);
-   const [adviceNum, setAdviceNum] = useState(undefined);
+   useEffect(() => {
+      showAdvice();
+   }, []);
 
    const showAdvice = () => {
-      let index = Math.floor(Math.random() * advice.length);
-
-      setNewAdvice(advice[index]);
-      setAdviceNum(index + 1);
+      axios
+         .get(`https://api.adviceslip.com/advice`)
+         .then((res) => setAdvice(res.data.slip));
    };
 
    return (
       <main className="app">
          <div className="container">
-            <h3 className="advice-num">advice #{!adviceNum ? 1 : adviceNum}</h3>
+            <h3 className="advice-num">advice #{advice?.id}</h3>
 
-            <p className="advice">{!newAdvice ? advice[0] : newAdvice}</p>
+            <p className="advice">"{advice?.advice}"</p>
 
             <div className="divider">
                <img src={divider} alt="" />
