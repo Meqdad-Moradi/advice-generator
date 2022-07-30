46e2a8f36587249d33627bb9ab361160b0cd3443
meqdad <meqdad.moradi@outlook.com>
2022-07-30 13:26:00 +0200

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import "./assets/style/style.css";
+import divider from "./assets/images/pattern-divider-desktop.svg";
+import dice from "./assets/images/icon-dice.svg";
+import { useState } from "react";
 
 function App() {
+   const [advice, setAdvice] = useState([
+      "It is easy to sit up and take notice, what is difficult is geting up and taking action.",
+      "lorem ",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "then",
+   ]);
+
+   const [newAdvice, setNewAdvice] = useState(null);
+   const [adviceNum, setAdviceNum] = useState(0);
+
+   const showAdvice = () => {
+      let index = Math.floor(Math.random() * 10);
+
+      setNewAdvice(advice[index]);
+      setAdviceNum(index + 1)
+   };
+
    return (
-      <>
-      </>
+      <main className="app">
+         <div className="container">
+            <h3 className="advice-num">advice #{adviceNum}</h3>
+
+            <h1 className="advice">{newAdvice}</h1>
+
+            <div className="divider">
+               <img src={divider} alt="" />
+            </div>
+
+            <button className="button" onClick={showAdvice}>
+               <img src={dice} alt="" />
+            </button>
+         </div>
+      </main>
    );
 }
 
